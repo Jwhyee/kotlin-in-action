b5f14aff2beaceb65b6fda5ddba95f6736455a70
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2023-12-14 10:55:30 +0900

    add : [chapter02] 코틀린 기초

---
+++ src/main/java/action/junyoung/chapter02/JavaPerson.java
@@ -0,0 +1,23 @@
+package action.chapter02;
+
+public class JavaPerson {
+    private final String name;
+    private boolean isMarried;
+
+    public JavaPerson(String name, boolean isMarried) {
+        this.name = name;
+        this.isMarried = isMarried;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+
+    public void setMarried(boolean married) {
+        isMarried = married;
+    }
+}
---
+++ src/main/java/action/junyoung/chapter02/PersonTest.java
@@ -0,0 +1,9 @@
+package action.chapter02;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        JavaPerson person = new JavaPerson("Bob", true);
+        System.out.println(person.getName());
+        System.out.println(person.isMarried());
+    }
+}
